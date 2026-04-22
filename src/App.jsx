5820d805e95d6595af8320c10feab6ec3d7927b2
Umm E Fatima <fatimaanis809@gmail.com>
2026-04-22 14:53:51 +0500

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', padding: '2rem' }}>
+        Hello Umm e Fatima 👋
+      </h1>
+    </main>
+  )
+}
+
+export default App
